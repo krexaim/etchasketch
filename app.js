a5347d9e3fca8d2d6dac0915ff8c417ed67c82a4
krexaim <krexaim@gmail.com>
2022-07-14 11:41:40 -0300

    grid barebones

---
+++ app.js
@@ -0,0 +1,14 @@
+// Create 16x16 grid 
+// https://stackoverflow.com/a/57550587/17677037
+const container = document.getElementById("container");
+
+function makeGrid(size) {
+  for (let i = 0; i < (size * size); i++) {
+    let cell = document.createElement("div");
+    cell.innerText = i + 1;
+    cell.classList.add("grid");
+    container.appendChild(cell);
+  };
+};
+
+makeGrid(16);
